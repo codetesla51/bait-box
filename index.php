25f2f00939f4b70a778441ec964cfbd1d94ac17d
codetesla <uoladele99@gmail.com>
2024-10-20 21:38:43 +0100

    modiffied index.php file

--- index.php
+++ index.php
@@ -7,28 +7,11 @@
 </head>
 <body>
     <h2>Enter Something</h2>
-    <form method="POST">
+    <form method="POST" action="process.php">
         <input type="text" name="user_name" placeholder="Enter Username" required>
         <input type="password" name="password" placeholder="Enter Password" required>
         <button type="submit" name="submit">Submit</button>
     </form>
 
-    <?php 
-    if (isset($_POST["submit"])) {
-        $user_name = $_POST["user_name"];
-        $user_pass = $_POST["password"];
-
-        // Concatenate user data properly
-        $data = "Data Fetched \n";
-        $data .= "Username: $user_name\n";
-        $data .= "Password: $user_pass\n";
-        $data .= "-------------------------------------\n";
-
-        // Write the data to input.txt
-        file_put_contents("input.txt", $data, FILE_APPEND);
-
-        echo "<p>Saved to input.txt</p>";
-    }
-    ?>
 </body>
 </html>
